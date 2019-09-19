bf094b3ab3e3ac35f23eceeec142611bef47f329
JimmyChen <31501225+jimmyo1123@users.noreply.github.com>
2019-09-20 02:20:59 +0930

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,25 @@
+/* POP UP */
+var popupContainer = document.getElementById('popUpContainer');
+
+const photoNodeList = document.getElementsByClassName('img-to-click');
+
+//loop through all photos
+const photoArr = Array.from(photoNodeList);
+photoArr.forEach(cur => cur.onclick = clicked_show);
+
+var popUpImg = document.getElementById('popupimg');
+var captionText = document.getElementById('caption');    
+
+function clicked_show(clicked) { /*detect the click*/
+    popupContainer.style.display = 'block';
+    popUpImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+//get span element
+var span = document.getElementsByClassName('close')[0];
+
+//when user click on span
+span.onclick = function() {
+    popupContainer.style.display = 'none';
+}
