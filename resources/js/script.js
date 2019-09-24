8a0264caded738907ea49132259f66c117f8eeee
Jimmy Chen <enjoy8062@gmail.com>
2019-09-24 22:40:51 +0930

    random photo order

--- resources/js/script.js
+++ resources/js/script.js
@@ -23,3 +23,43 @@ var span = document.getElementsByClassName('close')[0];
 span.onclick = function() {
     popupContainer.style.display = 'none';
 }
+
+/* RANDOM PHOTO ORDER */
+
+if (performance.navigation.type === 0 || performance.navigation.type === 1) {
+    var randomOrder = randomOrder(photoArr.length);
+    var i = 0;
+    while(i < photoArr.length){
+        photoArr[i].src = 'resources/img/food-' + randomOrder[i] +'.JPG';
+        i++;
+    }
+}
+
+function randomOrder(length){
+    var totalNum = getNumArray(length); //create num array 1~length
+    var randomNumArr = [];
+    for(var i = totalNum.length - 1;i >= 0; i--){
+        var randomNum = Math.floor(Math.random()*totalNum.length); //select random number in the array
+        randomNumArr.push(totalNum[randomNum]);
+        console.log(" totalNum[" +i + "]:" + totalNum[randomNum]);
+        console.log("randomNumArr: " + randomNumArr);
+        
+        totalNum.splice(randomNum, 1); //remove num in the array to avoid same result appear again
+        
+        console.log("totalNum.length: " + totalNum.length + " randomNum: " + randomNum);
+    }
+    return randomNumArr;
+}
+
+function getNumArray(length){
+    var nums = [];
+    for(var i = 1;i <= length ; i++){
+        nums.push(i);
+    }
+    return nums;
+}
+
+
+
+
+
